4f0b360f14c865e20958b90a2532b1ca6ba4d1dc
DianeLam <43661044+DianeLam@users.noreply.github.com>
2019-01-25 00:15:13 -0600

    Create p2.py
    Diane: This is what I have came up with so far for the parser, essentially it is just printing out the texts part so it is easier to read. It works on my computer.

---
+++ p2.py
@@ -0,0 +1,37 @@
+import math
+import re
+import math
+import sys
+import unittest
+import json
+
+data = []
+
+def load_data():
+	global data
+	f = open('gg2013.json', "r", encoding="utf8")
+	data = f.readlines()
+	f.close()
+	train(data)
+
+
+def train(lines):
+	x=[]
+	lines = json.dumps(lines)
+	line= lines.split('text')
+	#
+	for i in line:
+		line=json.dumps(i)
+		line.split("\\\":")
+		x.append(line)
+	#print((x[2]))
+	for i in x:
+		rest= i.split(',')[0]
+		print(rest)
+
+
+
+
+if __name__ == "__main__":
+	load_data()
+	
