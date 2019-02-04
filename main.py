1df2a7924d1da7a836818d5bd65fcd1316c13fcb
kimberlykwon <kimberlykwon2019@u.northwestern.edu>
2019-02-03 21:49:23 -0600

    removed stopwords

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import nltk
 from nltk.tokenize import TweetTokenizer
 from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk.corpus import stopwords
 
 #imports from other files
 from filter_tweets import filter_tweets
@@ -12,7 +13,7 @@ def main(param):
 
 	#get all tweets containing args[1] the regex search parameter.
 	#generally, we want to do search by the award name, then determine the winner from the results.
-	matches = filter_tweets(tweets, param, False);
+	matches = filter_tweets(tweets, param, False)
 
 	#tokenize each of the matches
 	#here, we create an array tokenized_matches where each index is a tokenized list of a tweet's text.
@@ -21,6 +22,13 @@ def main(param):
 	for match in matches:
 		tokenized_matches.append(tt.tokenize(match))
 
+	#remove stop words
+	stop_words = list(stopwords.words("english"))
+	sans_stopwords = [] #we could also remove from tokenized_matches
+	for token in tokenized_matches:
+		for word in token:
+			if word.lower() not in stop_words:
+				sans_stopwords.append(word)
 
 	#now we want to do stemming/lemmatization on the tokenized text
 	#stem(tokens), or something like that.
@@ -28,9 +36,8 @@ def main(param):
 	#try feature engineering with n-grams, etc.
 	#---some kind of function here---
 
-
-
 	print(tokenized_matches)
+	# print(sans_stopwords)
 	return tokenized_matches
 
 
