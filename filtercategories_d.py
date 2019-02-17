465c53dfec31e5dc8b2e9e8fdaddec7ff3beda89
DianeLam <43661044+DianeLam@users.noreply.github.com>
2019-02-16 22:45:34 -0600

    Create filtercategories_d.py

---
+++ filtercategories_d.py
@@ -0,0 +1,21 @@
+import re
+import sys
+from loaddatscategories_d import load_data
+
+
+def filter_tweets(tweets, param, caseSensitive=True):
+    matches =[]
+    for tweet in tweets:
+        text = tweet['text']
+        matches.append(tweet["text"])
+
+    return matches
+
+
+def main():
+    tweets = load_data()
+    return nameMatches
+
+
+if __name__ == "__main__":
+    main()
