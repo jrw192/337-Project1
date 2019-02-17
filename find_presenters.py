45ecc085961a0d85270c7b2022bc2265c0c7b7d4
Jodie Wei <jodiewei2019@u.northwestern.edu>
2019-02-17 17:09:22 -0600

    known people in presenters

--- find_presenters.py
+++ find_presenters.py
@@ -2,6 +2,8 @@ from filter_tweets import filter_tweets
 from find_awards import find_award
 from find_names import find_all_names  #takes in the raw text of a tweet, returns a list of actor/actress names identified from the tweet.
 from load_data import load_data
+import re
+
 
 
 
@@ -9,18 +11,29 @@ from load_data import load_data
 def find_presenters(tweet_list):
 	presenters = {} #presenters = { award_name : {people} }
 	#people = { person_name : count }
+	known_people = []
 
 
 	presenter_tweets = filter_tweets(tweet_list, 'present[a-z]*', caseSensitive=False)
 	print(len(presenter_tweets))
 	for tweet in presenter_tweets:
+		#print(tweet)
 		award = find_award(tweet)
 		if not award:
 			continue
-		known_people = list(presenters.keys())
-		people = find_all_names(tweet, known_people)
+
+		#we don't want names that come after the word "to" e.g. "presented to xxx"
+		toIndex = re.search(r'\bto\b', tweet)
+		if not toIndex:
+			toIndex = len(tweet)
+		else:
+			toIndex = toIndex.start()
+
+		people = find_all_names(tweet[:toIndex], known_people)
 		if not people:
 			continue
+			
+		known_people += people
 
 		if award in presenters:
 			curPeople = presenters[award]
@@ -41,18 +54,40 @@ def find_presenters(tweet_list):
 
 def associate_presenters_awards(tweet_list):
 	possible_presenters = find_presenters(tweet_list)
-	awards_presenters = {} #final result
+	final_presenters = {} #final result
+	print(possible_presenters.keys())
 
 	#TODO: find top 2 most mentioned people for each award, return as awards_presenters
+	for award in possible_presenters:
+		people = possible_presenters[award]
+		first ={}
+		second = {}
+		first = ["", 0]
+		second = ["", 0]
 
-	#return awards_presenters
+		for person in people: #find the first person
+			if people[person] > first[1]:
+				first = [person, people[person]]
+		for person in people: #find the second person
+			if person != first[0] and people[person] > second[1]:
+				second = [person, people[person]]
+		print("presenters for %s: %s and %s" % (award, first[0], second[0]))
+
+		presenters = [first[0], second[0]]
+		if second[0] == "":
+			presenters = [first[0]]
+		final_presenters[award] = presenters
+
+
+	print(final_presenters)
+	return final_presenters
 
 
 
 
 if __name__ == "__main__":
 	tweets = load_data()
-	associate_presenters_awards(tweets)
+	associate_presenters_awards(tweets[:50000])
 
 
 
