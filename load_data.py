b787cad6665f157d0f77c35dbf544c77bcb4a18f
Jodie Wei <jodiewei2019@u.northwestern.edu>
2019-02-17 22:20:26 -0600

    load by year

--- load_data.py
+++ load_data.py
@@ -1,11 +1,21 @@
 import json
 import sys
-def load_data(filename='data/gg2013.json'):
+import os
+
+def load_data(year):
 	tweets = []
+	filename = ""
+	files = os.listdir('data')
+	print(files)
+	for file in files:
+		if year in file:
+			filename = 'data/' + file
+			break
+
 	with open(filename) as f:
 		tweets = json.load(f)
 	f.close()
 	return tweets
 
 if __name__ == "__main__":
-	load_data('data/gg2013.json')
+	load_data('2015')
