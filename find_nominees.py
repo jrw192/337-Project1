2e0bad110496f57d07129beee01db9bcbe1b1302
kimberlykwon <kimberlykwon2019@u.northwestern.edu>
2019-02-18 00:26:33 -0600

    human readable

--- find_nominees.py
+++ find_nominees.py
@@ -8,7 +8,6 @@ import re
 from process_tweets import lemmatize_tweet, stem_tweet
 from find_films import *
 from filter_tweets import filter_tweets_by_time, filter_tweets_text
-from gg_api import OFFICIAL_AWARDS_1315 
 from find_award_time import *
 
 from load_data import load_data
@@ -26,19 +25,18 @@ def find_nominees(tweet):
 	text = clean_tweet(tweet).lower()
 	text = tt.tokenize(text)
 
-	nomin = str("nomin")
-	nomin_found = False
+	# nomin = str("nomin")
+	# nomin_found = False
 
-  # stem all
-	stemmed = stem_tweet(text)
-	if "nomin" in stemmed or "nomine" in stemmed:
-		nomin_found = True
+  # # stem all
+	# stemmed = stem_tweet(text)
+	# if "nomin" in stemmed or "nomine" in stemmed:
+	# 	nomin_found = True
 
 	nominee_list = []
-	if nomin_found:
-		nominee_list = find_all_names(tweet)
+	# if nomin_found:
+	nominee_list = find_all_names(tweet)
 
-	
 	return nominee_list
 	
 
@@ -57,7 +55,6 @@ def find_all_nominees(tweet_list, awards_list):
 
 	return nominees
 			
-
 def find_tweets_of_award(tweet_list, award):
 	award_stripped = award
 	for word in stop_words:
@@ -77,7 +74,7 @@ if __name__ == "__main__":
 	raw_data = load_data('2013')
 	time = find_start_time(raw_data, "golden globes")
 	rel_tweets = filter_tweets_by_time(raw_data, time)
-	print(find_all_nominees(rel_tweets, OFFICIAL_AWARDS_1315))
+	print(find_all_nominees(rel_tweets, ['cecil b. demille award', 'best motion picture - drama', 'best performance by an actress in a motion picture - drama', 'best performance by an actor in a motion picture - drama', 'best motion picture - comedy or musical', 'best performance by an actress in a motion picture - comedy or musical', 'best performance by an actor in a motion picture - comedy or musical', 'best animated feature film', 'best foreign language film', 'best performance by an actress in a supporting role in a motion picture', 'best performance by an actor in a supporting role in a motion picture', 'best director - motion picture', 'best screenplay - motion picture', 'best original score - motion picture', 'best original song - motion picture', 'best television series - drama', 'best performance by an actress in a television series - drama', 'best performance by an actor in a television series - drama', 'best television series - comedy or musical', 'best performance by an actress in a television series - comedy or musical', 'best performance by an actor in a television series - comedy or musical', 'best mini-series or motion picture made for television', 'best performance by an actress in a mini-series or motion picture made for television', 'best performance by an actor in a mini-series or motion picture made for television', 'best performance by an actress in a supporting role in a series, mini-series or motion picture made for television', 'best performance by an actor in a supporting role in a series, mini-series or motion picture made for television']))
 
 
 
